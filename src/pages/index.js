ad2b27e0f4e074b6a48ea5f842ad0be213d5eae6
Oliver-Turp <tcgamingbusiness@gmail.com>
2023-04-14 21:48:08 +0100

    Added ENV File

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,79 @@ import Layout from '@components/Layout';
 import Container from '@components/Container';
 import Button from '@components/Button';
 
-import images from '@data/images';
+import { search, mapImageResources, getFolders } from "@lib/cloudinary";
 
-import styles from '@styles/Home.module.scss'
+import styles from "@styles/Home.module.scss";
 
-export default function Home() {
+export default function Home({
+  images: defaultImages,
+  nextCursor: defaultNextCursor,
+  totalCount: defaultTotalCount,
+  folders,
+}) {
+  const [images, setImages] = useState(defaultImages);
+  const [nextCursor, setNextCursor] = useState(defaultNextCursor);
+  const [totalCount, setTotalCount] = useState(defaultTotalCount);
+  const [activeFolder, setActiveFolder] = useState("");
+
+  async function handleLoadMore(event) {
+    event.preventDefault();
+
+    const results = await fetch("/api/search", {
+      method: "POST",
+      body: JSON.stringify({
+        expression: `folder=""`,
+        nextCursor,
+      }),
+    }).then((r) => r.json());
+
+    const {
+      resources,
+      next_cursor: updatedNextCursor,
+      total_count: updatedTotalCount,
+    } = results;
+
+    const images = mapImageResources(resources);
+
+    setImages((prev) => {
+      return [...prev, ...images];
+    });
+    setNextCursor(updatedNextCursor);
+    setTotalCount(updatedTotalCount);
+  }
+
+  function handleOnFolderClick(event) {
+    const folderPath = event.target.dataset.folderPath;
+    setActiveFolder(folderPath);
+    setNextCursor(undefined);
+    setImages([]);
+    setTotalCount(0);
+  }
+
+  useEffect(() => {
+    (async function run() {
+      const results = await fetch("/api/search", {
+        method: "POST",
+        body: JSON.stringify({
+          nextCursor,
+          expression: `folder="${activeFolder || ""}"`,
+        }),
+      }).then((r) => r.json());
+
+      const {
+        resources,
+        next_cursor: nextPageCursor,
+        total_count: updatedTotalCount,
+      } = results;
+
+      const images = mapImageResources(resources);
+
+      setImages(images);
+      setNextCursor(nextPageCursor);
+      setTotalCount(updatedTotalCount);
+    })();
+  }, [activeFolder]);
+
   return (
     <Layout>
       <Head>
@@ -21,25 +89,70 @@ export default function Home() {
       <Container>
         <h1 className="sr-only">My Images</h1>
 
+        <h2 className={styles.header}>Folders</h2>
+
+        <ul className={styles.folders} onClick={handleOnFolderClick}>
+          {folders.map((folder) => {
+            return (
+              <li key={folder.path}>
+                <button data-folder-path={folder.path}>{folder.name}</button>
+              </li>
+            );
+          })}
+        </ul>
+
         <h2 className={styles.header}>Images</h2>
 
         <ul className={styles.images}>
-          {images.map(image => {
+          {images.map((image) => {
             return (
               <li key={image.id}>
                 <a href={image.link} rel="noreferrer">
                   <div className={styles.imageImage}>
-                    <Image width={image.width} height={image.height} src={image.image} alt="" />
+                    <Image
+                      width={image.width}
+                      height={image.height}
+                      src={image.image}
+                      alt=""
+                    />
                   </div>
-                  <h3 className={styles.imageTitle}>
-                    { image.title }
-                  </h3>
+                  <h3 className={styles.imageTitle}>{image.title}</h3>
                 </a>
               </li>
-            )
+            );
           })}
         </ul>
+        {totalCount > images.length && (
+          <p>
+            <Button onClick={handleLoadMore}>Load More Results</Button>
+          </p>
+        )}
       </Container>
     </Layout>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const results = await search({
+    expression: "folder=''",
+  });
+
+  const {
+    resources,
+    next_cursor: nextCursor,
+    total_count: totalCount,
+  } = results;
+
+  const images = mapImageResources(resources);
+
+  const { folders } = await getFolders();
+
+  return {
+    props: {
+      images,
+      nextCursor: nextCursor || false,
+      totalCount,
+      folders,
+    },
+  };
 }
